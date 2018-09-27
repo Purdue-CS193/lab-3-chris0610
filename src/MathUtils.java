d30e77234d63f56560d5a78469d06f4955fcc027
chris0610 <21194612+chris0610@users.noreply.github.com>
2018-09-27 19:43:54 -0400

    Update MathUtils.java

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -1,46 +1,45 @@
-public class StringUtils {
+public class MathUtils {
     /**
-     * makePurdueUsername -- <= 8-letter-long lowercase username creator
-     * Usernames may be AT MOST 8 letters in length, and should
-     * consist of the first letter of one's first name, and the 
-     * amount of their last name that will fit into the remaining
-     * 7 characters. All should be lowercase.
-     * Ex: a = "Sean ", b = "Flannery", returns "sflanner"
+     * divideNumbers -- a / b
+     * Divide a by b and return the result
      */
-    public static String makePurdueUsername(String firstName, String lastName) {
-        /* Generate the long version of the name first:
-            Ex: firstName = "Sean", lastName = "Flannery"
-                    --> res = "SFlannery"
-         */
-        String res = firstName.substring(0,1) + lastName;
-        /* Shorten the string to 8 characters
-            Ex: res = "SFlannery"
-                    --> res = "SFlanner"
-         */
-        if (lastName.length() < 7) {
-            return res.toLowerCase();
-        } else {
-            res = firstName.substring(0,1) + lastName.substring(0, 7);
-            return res.toLowerCase();
+    public static double divideNumbers(double dividend, double divisor) {
+        /* Do some casual division ... Super easy ... Shouldn't be any problems here */
+        double res = dividend / divisor;
+        /* Return the DEFINITELY CORRECT result */
+        return res;
+    }
+
+    /**
+     * raiseToPower -- (base)^(exp)
+     * Raise the number base to power exp
+     */
+    public static double raiseToPower(int base, int exp) {
+        /* Set initial result to be just the number */
+        double res = base;
+
+        /* Determine if the exponent is negative */
+        boolean isNegative = false;
+        if (exp < 0) {
+            exp *= -1;
+            isNegative = true;
         }
-        /* Convert the name to lower-case
-            Ex: res = "SFlanner"
-                    --> res = "sflanner"
-         */
 
-        /* return the result of our calculation */
+        /* Multiply the result by the base exp times
+            Ex: base = 2, exp = 2
+               --> res = 2, then res = 4 */
+        for (int i = 0; i < exp-1; i++) {
+           res *= base;
+        }
 
-    }
-
-    /**
-     * replaceStudentUsername -- replace student usernames in sensitive text
-     * We want to make sure student information is expunged from sensitive
-     * documents as much as possible. 
-     * Ex: text = "jframes was among the students whose SSNs were leaked.",
-     * username = "[DATA EXPUNGED] was among the students whose SSNs were leaked.",
-     */
-    public static String replaceStudentUsername(String text, String username) {
-        /* Replace every instance of the username with proper message */
-        return text.replaceAll(username, "[DATA EXPUNGED]");
-    }
+        /* If it's a negative exponent, we should invert it! */
+        if (isNegative) {
+            double dividedRes = divideNumbers(1, res);
+            return dividedRes;
+        }
+        /* Otherwise, we are safe to just return the result */
+        else {
+            return res;
+        }
+    }    
 }
